329874fc389fd449707dd64c175a31f1f19cbb85
abdul <abdul.tanveer@gmail.com>
2022-02-08 20:29:17 +0530

    get value from edittext

--- app/src/main/java/com/abdul/firstcogn/MainActivity.java
+++ app/src/main/java/com/abdul/firstcogn/MainActivity.java
@@ -5,15 +5,20 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText nameEditText; //declaration
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); //inflation
+        nameEditText = findViewById(R.id.etName); //initialized it
+
+
 
         TextView mTextView = new TextView(this);
         mTextView.setText("text from java");
@@ -23,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void clickHandler(View view) {
-        Toast.makeText(this, "welcome to android", Toast.LENGTH_SHORT).show();
+
+        String value = nameEditText.getText().toString();
+        Toast.makeText(this, value, Toast.LENGTH_SHORT).show();
     }
 }
