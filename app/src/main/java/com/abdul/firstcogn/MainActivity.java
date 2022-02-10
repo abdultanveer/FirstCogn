4e21ddbe8a982f1da469ee6b4be2dfb692c45885
abdul <abdul.tanveer@gmail.com>
2022-02-10 22:18:06 +0530

    arrays.xml

--- app/src/main/java/com/abdul/firstcogn/MainActivity.java
+++ app/src/main/java/com/abdul/firstcogn/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.AlarmClock;
