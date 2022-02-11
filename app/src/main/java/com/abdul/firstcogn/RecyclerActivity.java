26ac095af636ceb9281cb37398f054a8c3be69b4
abdul <abdul.tanveer@gmail.com>
2022-02-11 20:35:27 +0530

    recyclerview -3[data]

--- app/src/main/java/com/abdul/firstcogn/RecyclerActivity.java
+++ app/src/main/java/com/abdul/firstcogn/RecyclerActivity.java
@@ -5,7 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class RecyclerActivity extends AppCompatActivity {
-
+String[] languages = new String[] {"english","hindi","french","arabic",
+                                        "english","hindi","french","arabic"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
