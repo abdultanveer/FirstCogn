99203a1616fcf9929a4444d8d2d899109c7f094f
abdul <abdul.tanveer@gmail.com>
2022-02-11 20:38:46 +0530

    recyclerview -4c implement adapter methods

--- app/src/main/java/com/abdul/firstcogn/LanguageAdapter.java
+++ app/src/main/java/com/abdul/firstcogn/LanguageAdapter.java
@@ -1,8 +1,27 @@
 package com.abdul.firstcogn;
 
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class LanguageAdapter extends RecyclerView.Adapter<LangViewHolder> {
+public class LanguageAdapter extends RecyclerView.Adapter<LanguageAdapter.LangViewHolder> {
+
+    @NonNull
+    @Override
+    public LangViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull LangViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 
     class LangViewHolder{}
 }
