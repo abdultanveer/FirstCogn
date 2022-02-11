f7848c92919f74aae232c892e65d050539c3dac7
abdul <abdul.tanveer@gmail.com>
2022-02-11 22:13:54 +0530

    recyclerview -4g - logging to trace

--- app/src/main/java/com/abdul/firstcogn/LanguageAdapter.java
+++ app/src/main/java/com/abdul/firstcogn/LanguageAdapter.java
@@ -1,5 +1,6 @@
 package com.abdul.firstcogn;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,27 +12,37 @@ import androidx.recyclerview.widget.RecyclerView;
 import kotlin.jvm.internal.Lambda;
 
 public class LanguageAdapter extends RecyclerView.Adapter<LanguageAdapter.LangViewHolder> {
+    public static  String TAG = LanguageAdapter.class.getSimpleName();
+
     String[] mLanguages;
     public LanguageAdapter(String[] languages) {
         mLanguages = languages;
+        Log.i(TAG,"constructor");
     }
 
     @NonNull
     @Override //ayman - buy the row planks from market
     public LangViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.i(TAG,"onCreateViewHolder - ayman buying a plank");
+
         View rowView = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_plank,parent,false);
         return new LangViewHolder(rowView);
     }
 
     @Override //john -- handwriting -- write the data on the planks textview
     public void onBindViewHolder(@NonNull LangViewHolder holder, int position) {
+        Log.i(TAG,"onBindViewHolder - john writing on plank--" + mLanguages[position]);
+
         holder.rowTextview.setText(mLanguages[position]);
 
     }
 
     @Override //christine -- keep the count of no items in the data
     public int getItemCount() {
+        Log.i(TAG,"getItemCount - christine keeping a count");
+
         return mLanguages.length;
+
     }
 
     //derek -- maintains the box containing row planks
@@ -39,6 +50,8 @@ public class LanguageAdapter extends RecyclerView.Adapter<LanguageAdapter.LangVi
         TextView rowTextview;
         public LangViewHolder(@NonNull View rowViewPlank) {
             super(rowViewPlank);
+            Log.i(TAG,"LangViewHolder - derek maintaining viewholder");
+
             rowTextview = rowViewPlank.findViewById(R.id.tvRow);
 
         }
--- app/src/main/java/com/abdul/firstcogn/RecyclerActivity.java
+++ app/src/main/java/com/abdul/firstcogn/RecyclerActivity.java
@@ -8,10 +8,12 @@ import android.os.Bundle;
 
 public class RecyclerActivity extends AppCompatActivity {
 String[] languages = new String[] {"english","hindi","french","arabic",
-                                        "english","hindi","french","arabic"};
+                                   "english","hindi","french","arabic",
+        "english","hindi","french","arabic", "english","hindi","french","arabic", "english","hindi","french","arabic"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_recycler);
 
         RecyclerView lRecyclerView = findViewById(R.id.lrecycler_view);
