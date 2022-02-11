e3dfdc5c9c9031c85e676daf2f3345b6eadebc28
abdul <abdul.tanveer@gmail.com>
2022-02-12 00:51:51 +0530

    shape gradient

--- app/src/main/java/com/abdul/firstcogn/RecyclerActivity.java
+++ app/src/main/java/com/abdul/firstcogn/RecyclerActivity.java
@@ -1,10 +1,12 @@
 package com.abdul.firstcogn;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class RecyclerActivity extends AppCompatActivity {
 String[] languages = new String[] {"english","hindi","french","arabic",
@@ -18,8 +20,15 @@ String[] languages = new String[] {"english","hindi","french","arabic",
 
         RecyclerView lRecyclerView = findViewById(R.id.lrecycler_view);
         LanguageAdapter adapter = new LanguageAdapter(languages);
-        lRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        lRecyclerView.setLayoutManager(new GridLayoutManager(this,2));
         lRecyclerView.setAdapter(adapter);
 
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Toast.makeText(this, "show dialog to close", Toast.LENGTH_SHORT).show();
     }
 }
