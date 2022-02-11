e1a96350d4a35cba32fe7215aee288ef619f5c61
abdul <abdul.tanveer@gmail.com>
2022-02-11 20:37:55 +0530

    recyclerview -4b

--- app/src/main/java/com/abdul/firstcogn/LanguageAdapter.java
+++ app/src/main/java/com/abdul/firstcogn/LanguageAdapter.java
@@ -3,4 +3,6 @@ package com.abdul.firstcogn;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class LanguageAdapter extends RecyclerView.Adapter<LangViewHolder> {
+
+    class LangViewHolder{}
 }
